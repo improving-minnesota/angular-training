8c1ae677610fdc5bb318e389b28e272646a9e193
Bruce Coddington <brucecoddington@gmail.com>
2014-03-01 17:57:24 -0600

    moving all server stuff into an 'api' directory to avoid confusion

--- timesheet/api/config/environments/production.js
+++ timesheet/api/config/environments/production.js
@@ -6,9 +6,9 @@ module.exports = function () {
   this.set('securePort', properties.server.prod.securePort);
 
   // Serve static content
-  this.use(express.static(__dirname + "/../../client/dist/timesheet"));
+  this.use(express.static(__dirname + "/../../../client/dist/timesheet"));
 
   this.use(function (req, res) {
     res.send(404);
   });
-}
+};
