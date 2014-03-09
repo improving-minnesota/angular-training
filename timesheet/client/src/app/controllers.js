63aabcc38e4d4246abffe5c8280851048b3da558
Bruce Coddington <brucecoddington@gmail.com>
2014-03-09 18:31:05 -0500

    removed angular stuff out of index.jade removed everything angular from lab 2

--- timesheet/client/src/app/controllers.js
+++
@@ -1,3 +0,0 @@
-// TODO : create the app.controllers module
-// 1. create a MainCtrl with an empty body
-// 2. create an AppCtrl that sets demo to an empty object on scope. 
--- timesheet/client/src/main.js
+++
@@ -1,4 +0,0 @@
-// TODO : configure the main module
-// 1. add both template modules as dependencies
-// 2. add a dependency on the app module
-// 3. add a run block to log to console that the application is running. 
