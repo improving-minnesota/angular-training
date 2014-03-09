21f028582d401e9a78a8bfde1437fe624f4a9133
Bruce Coddington <brucecoddington@gmail.com>
2014-03-09 17:53:03 -0500

    removed module lab stuff and set up the index.html for demo

--- timesheet/client/src/app/app.js
+++ timesheet/client/src/app/app.js
@@ -1,19 +1,15 @@
 angular.module('app', [
-  'app.resources',
   'app.controllers',
-  'app.employees',
-  'app.projects',
-  'app.timesheets',
   'ngRoute'
 ])
 
 .config(function ($routeProvider) {
 
-  // TODO : Register the application routes with $routeProvider
-  // 1. /projects : projects/index.html template and ProjectCtrl
-  // 2. /employees : employees/index.html template and EmployeeCtrl
-  // 3. /timesheets : timesheets/index.html template and TimesheetCtrl
-  // 4. default other routes to /projects
-  // 5. the base url for all templates is 'assets/templates/app'
+  $routeProvider
+    .when("/home", {
+      templateUrl: 'assets/templates/index.html',
+      controller: 'AppCtrl'
+    })
+    .otherwise('/home');
 });
   
--- timesheet/client/src/app/projects/projects.js
+++
@@ -1,9 +0,0 @@
-angular.module('app.projects', [
-  'app.projects.controllers'
-])
-
-.run(function ($api) {
-
-  // TODO : register the projects resource with the $api service
-  
-});
